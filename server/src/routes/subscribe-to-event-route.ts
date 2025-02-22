712fe874e3f9820a3337b9baabc0d52bcb176dcf
allefcampos <allef-cleiton88@hotmail.com>
2025-02-22 14:39:15 -0300

    Aula 02 - Integração com banco de dados e primeiras funcionalidades
    Nessa aula, vamos realizar a conexão da nossa API com o banco de dados Postgres e também o Redis, que será responsável pelas funcionalidades de Ranking, após isso, vamos desenvolver as rotas de inscrição no evento, convite por meio de um inscrito e contabilização de clicks nessa rota.

--- server/src/routes/subscribe-to-event-route.ts
+++ server/src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
   app.post(
@@ -15,8 +16,7 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
         }),
         response: {
           201: z.object({
-            name: z.string(),
-            email: z.string(),
+            subscriberId: z.string(),
           }),
         },
       },
@@ -24,9 +24,14 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
     async (request, reply) => {
       const { name, email } = request.body as { name: string; email: string }
 
-      return reply.status(201).send({
+      const { subscriberId } = await subscribeToEvent({
         name,
         email,
+        invitedBySubscriberId: null,
+      })
+
+      return reply.status(201).send({
+        subscriberId,
       })
     }
   )
